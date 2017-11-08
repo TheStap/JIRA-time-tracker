f5adb6096038c0d1e69d796ece9752490028235e
Artem Malikov <ineveraskedforthis@bk.ru>
2017-11-08 23:01:24 +0400

    start proxy method cookie-based authorisation in jira

--- app.js
+++ app.js
@@ -10,47 +10,74 @@ const index = require('./routes/index');
 const users = require('./routes/users');
 
 const app = express();
-const winston = require('winston');
-const log = require('./libs/log')(module);
-log.error('Hello again distributed logs');
+const axios = require('axios');
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	indentedSyntax: true,
+	sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/test', (req, res, next) => {
+	res.send('hi!');
+});
+
+app.post('/login', (req, res, next) => {
+	let body = req.body;
+	if (body.password && body.login) {
+		axios.post('https://jira.goods.ru/rest/auth/1/session',
+			{
+				username: body.login,
+				password: body.password
+			},
+			{
+				headers: {
+					'content-type': 'application/json'
+				}
+			}).then(r => {
+				if (r.status === '200')
+				{
+
+				}
+			console.log(r.response);
+		}).catch(e => {
+			res.status(e.response.status);
+			res.send({error: e.response.data.errorMessages})
+		})
+	}
+	else {
+		res.status(401);
+		res.json({err: 'Password and login required'});
+	}
+});
+
+
+app.use(function (req, res, next) {
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
